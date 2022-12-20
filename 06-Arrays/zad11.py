e381a4cb81b7291302c4f95279eae38029395e70
ciula12 <ciula.wawrzyniec@gmail.com>
2022-12-20 17:11:34 +0100

    20/12/2022

---
+++ 06-Arrays/zad11.py
@@ -0,0 +1,9 @@
+'''
+Create a program that displays the name of month for a given month number (1 to 12). 
+Define a month(n) function that returns the name of month for the number n. Store month names in an array. 
+Using defined function, display month names for the following month numbers: 1, 2, 11, 12.
+'''
+
+def month(n):
+    months["sty", "lut", "mar", "kwie", "maj", "czer", "lip", "sie", "wrze", "paz", "gru"]
+    return
---
+++ Shapes/shapes.py
@@ -0,0 +1,29 @@
+from turtle import *
+import turtle as t
+
+t.title("Wawrzyniec Ciuła")
+color('red', 'yellow')
+
+
+screen = t.Screen()
+screen.setup(width=1.0, height=1.0)
+print(t.screensize())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+t.done()
+
